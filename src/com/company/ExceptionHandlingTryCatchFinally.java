ffa3b270da4283936fb585f03b17f7b57aec3345
Pranavi <pranavi.mittapalli2000@gmail.com>
2021-07-17 17:59:19 +0530

    commit

---
+++ src/com/company/ExceptionHandlingTryCatchFinally.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class ExceptionHandlingTryCatchFinally
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int i = 7;
+            int j = 0;
+            int k = i/j;
+            System.out.println("output is " + k);
+
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Error ");
+        }
+        finally
+        {
+            System.out.println("Bye");
+        }
+    }
+}
